e536b8ccf7fdd9947eaf1a022b23a99285834b1e
driiisdev <olayinkayakub01@gmail.com>
2024-01-20 12:24:54 +0100

    paginate

--- app/routes/explore.py
+++ app/routes/explore.py
@@ -1,6 +1,6 @@
 from app import app
 from app.models import Post
-from flask import request, render_template
+from flask import request, render_template, url_for
 from flask_login import login_required
 
 @app.route('/explore')
@@ -9,4 +9,9 @@ def explore():
   page = request.args.get('page', 1, type=int)
   posts = Post.query.order_by(Post.timestamp.desc()).paginate(
     page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-  return render_template('index.html', title='Explore', posts=posts)
+  next_url = url_for('explore', page=posts.next_num) \
+    if posts.has_next else None
+  prev_url = url_for('explore', page=posts.prev_num) \
+    if posts.has_prev else None
+  return render_template("index.html", title='Explore', 
+    posts=posts.items, next_url=next_url, prev_url=prev_url)
